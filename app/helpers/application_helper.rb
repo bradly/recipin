000b764d7127e454febb2af4f71a1b7b23fd06c6
Bradly Feeley <4227+bradly@users.noreply.github.com>
2024-02-08 08:01:52 -0800

    Only add flex classes to icons when text is present.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,10 @@ module ApplicationHelper
     tag.span class: 'absolute inset-x-0 -top-px bottom-0'
   end
 
-  def icon_for(name, text: nil, size: 'lg', variant: 'regular', icon_class: nil, wrapper_class: nil)
-    tag.span class: "flex gap-2 items-center #{wrapper_class}" do
+  def icon_for(name, text: nil, size: 'lg', variant: 'regular', icon_class: nil, wrapper_class: '')
+    wrapper_class += 'flex gap-2 items-center' if text.present?
+
+    tag.span class: wrapper_class do
       icon "fa-#{variant}", name.to_s.dasherize, text, class: "fa-#{size} #{icon_class}"
     end
   end
