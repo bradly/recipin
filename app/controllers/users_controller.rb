76b4601332e979d74c40fa2d5b51c8c6aa4cae4c
Bradly Feeley <4227+bradly@users.noreply.github.com>
2024-02-19 07:31:29 -0800

    Adding flash messages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
-class UsersController < ResourceController
-  def permitted_params
-    [:email, :password]
+class UsersController < Clearance::UsersController
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+
+    if @user.update(permitted_params)
+      redirect_to profile_path, notice: 'User saved'
+    else
+      render :edit, alert: 'There was a problem saving the account'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "recipes#index"
   resources :recipes
-  resources :users
+  get 'profile', to: 'profile#show', as: :profile
 
   STATIC_PAGES.each do |page|
     get page, to: "pages##{page}", as: page
