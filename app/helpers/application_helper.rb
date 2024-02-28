78d92c3eeb0aa76c1120dd12df999e71f582fa4f
Bradly Feeley <4227+bradly@users.noreply.github.com>
2024-02-27 16:17:11 -0800

    Cleaning up sign in and sign up forms
    Still need to do form validation.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,8 +19,8 @@ module ApplicationHelper
     tag.span class: 'absolute inset-x-0 -top-px bottom-0'
   end
 
-  def icon_for(name, text: nil, size: 'lg', variant: 'regular', icon_class: nil, wrapper_class: '')
-    wrapper_class += 'flex gap-2 items-center' if text.present?
+  def icon_for(name, text: nil, gap: 3, size: 'lg', variant: 'regular', icon_class: nil, wrapper_class: '')
+    wrapper_class += " flex gap-#{gap} items-center" if text.present?
 
     tag.span class: wrapper_class do
       icon "fa-#{variant}", name.to_s.dasherize, text, class: "fa-#{size} #{icon_class}"
