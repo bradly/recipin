4486ab91c4ad2937cfe7e771028d0f157c6ce77d
Bradly Feeley <4227+bradly@users.noreply.github.com>
2024-03-01 14:24:56 -0800

    Adding gap-3 to tailwind safelist.

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -19,5 +19,8 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
+  ],
+  safelist: [
+    'gap-3'
   ]
 }
